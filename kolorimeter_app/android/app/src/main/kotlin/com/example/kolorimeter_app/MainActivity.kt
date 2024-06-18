692ad179ba0b5e5898084755906c70f0df7c099a
Dwi Kurniawan <dwi.kurniawan@untidar.ac.id>
2024-06-18 16:41:51 +0700

    Update 18_06_24
    1.appBar ditambah container untuk pattern dan gradien
    2. Title appBar menggunakan style stroke menggunakan teknik stack salah satu foreground pake paint()
    3. App icon menggunakan assets

---
+++ kolorimeter_app/android/app/src/main/kotlin/com/example/kolorimeter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kolorimeter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ kolorimetri/android/app/src/main/kotlin/com/example/kolorimetri/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kolorimetri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
