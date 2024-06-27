11572fa6cc15433905270b16b55ed55fcdb811b1
Dwi Kurniawan <80032665+kurniawanUntidar@users.noreply.github.com>
2024-06-27 08:05:37 +0700

    update 27_06_24
    kolorcap

--- kolorcap/lib/pages/camera_page.dart
+++ kolorcap/lib/pages/camera_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:camerawesome/camerawesome_plugin.dart';
+import 'package:camerawesome/pigeon.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -14,13 +15,56 @@ class CameraPage extends StatelessWidget {
         color: Colors.white,
         child: CameraAwesomeBuilder.awesome(saveConfig: SaveConfig.photoAndVideo(
           initialCaptureMode: CaptureMode.photo,
-        //   photoPathBuilder: (sensors) async{
-        //     final Directory extDir = await getTemporaryDirectory();
-        //     final testDir =await Directory(
-        //       '${extDir.path}/camerawesome',
-        //     ).create(recursive: true);
-        //  },
-        )
+          photoPathBuilder: (sensors) async{
+            final Directory extDir = await getTemporaryDirectory();
+            final testDir =await Directory(
+              '${extDir.path}/camerawesome',
+            ).create(recursive: true);
+            if(sensors.length == 1){
+              final String filePath = '${testDir.path}/${DateTime.now().millisecondsSinceEpoch}.jpg';
+              return SingleCaptureRequest(filePath, sensors.first);
+            }else{
+              // Separate pictures taken with front and back camera
+                return MultipleCaptureRequest(
+                  {
+                    for (final sensor in sensors)
+                      sensor:
+                          '${testDir.path}/${sensor.position == SensorPosition.front ? 'front_' : "back_"}${DateTime.now().millisecondsSinceEpoch}.jpg',
+                   },
+                );
+              }
+            },
+            videoOptions: VideoOptions(enableAudio: true,
+            android: AndroidVideoOptions(bitrate:6000000,
+            fallbackStrategy: QualityFallbackStrategy.lower,
+             ),
+             ),
+             exifPreferences: ExifPreferences(saveGPSLocation: true),
+          ),
+          sensorConfig: SensorConfig.single(
+            sensor: Sensor.position(SensorPosition.back),
+            flashMode: FlashMode.auto,
+            aspectRatio: CameraAspectRatios.ratio_4_3,
+            zoom: 0.0,
+          ),
+           enablePhysicalButton: true,
+           previewAlignment: Alignment.center,
+           previewFit: CameraPreviewFit.contain,
+           onMediaTap: (MediaCapture){
+            MediaCapture.captureRequest.when(
+              single: (single){
+                debugPrint('single: ${single.file?.path}');
+                single.file?.openRead();
+              },
+              multiple: (multiple){
+                multiple.fileBySensor.forEach((key, value){
+                  debugPrint('Multiple file token: $key ${value?.path}');
+                  value?.openRead();
+                });
+              },
+            );
+          },
+          availableFilters: awesomePresetFiltersList,
         ),
       ),
     );
