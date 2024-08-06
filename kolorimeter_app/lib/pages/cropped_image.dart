d22aa4695ab24dc7e50c5262f099c04dbeca2031
Dwi Kurniawan <dwi.kurniawan@untidar.ac.id>
2024-08-06 11:10:35 +0700

    Update 06_08_24
    1. Add dependencies <path_provider:^2.1.4> and <flutter_file_dialog:^3.0.2> on pubspec.yaml file
    2. Add assets [-assets/images/1.png ... 5.png] on pubspec.yaml as image refference
    3. Add function <saveImage(imageToSave)> on cropped_image.dart -> still have a bug
    4.  Add ElevatedButton to trigger saveImage(imageToSave) function

--- kolorimeter_app/lib/pages/cropped_image.dart
+++ kolorimeter_app/lib/pages/cropped_image.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:image_cropper/image_cropper.dart';
 
 class CroppedImage extends StatefulWidget {
@@ -12,6 +13,24 @@ class CroppedImage extends StatefulWidget {
 }
 
 class _CroppedImageState extends State<CroppedImage> {
+  void saveImage(CroppedFile imageToSave) async {
+    String? message;
+    try {
+      final params = SaveFileDialogParams(sourceFilePath: imageToSave.path);
+      final finalPath = await FlutterFileDialog.saveFile(params: params);
+      if (finalPath != null) {
+        message = "Save to disk";
+      }
+    } catch (e) {
+      message = "An error while save image";
+    }
+    if (message != null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(message),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,14 +41,26 @@ class _CroppedImageState extends State<CroppedImage> {
         foregroundColor: Colors.white,
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: InteractiveViewer(
-              child: Image(
-            image: FileImage(
-              File(widget.image.path),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: InteractiveViewer(
+                  child: Image(
+                image: FileImage(
+                  File(widget.image.path),
+                ),
+              )),
             ),
-          )),
+            ElevatedButton(
+                onPressed: () {
+                  saveImage(widget.image);
+                },
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: const Color.fromARGB(255, 32, 2, 2)),
+                ))
+          ],
         ),
       ),
     );
